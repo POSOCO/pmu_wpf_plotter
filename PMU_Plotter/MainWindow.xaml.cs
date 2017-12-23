43ac6c58800878b8e496fd154c3947cebcc61511
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-12-23 23:41:53 +0530

    menu options and scrollview

--- PMU_Plotter/MainWindow.xaml.cs
+++ PMU_Plotter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,15 +25,43 @@ namespace PMU_Plotter
         {
             InitializeComponent();
             String str = (String)((App)Application.Current).Properties["ArbitraryArgName"];
-            if(str != null)
-            {
-                WelcomeText.Text = str;
-            }            
+            openFileName(str);
         }
 
-        public void setWelcomeText(string str)
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            WelcomeText.Text = str;
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            // MessageBox.Show("You clicked 'Open...'");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileNames[0];
+                openFileName(filename);
+                    
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("You clicked 'Exit...'");
+            Application.Current.Shutdown();
+        }
+
+        private void openFileName(string str)
+        {
+            if (str != null)
+            {
+                string[] lines = System.IO.File.ReadAllLines(str);
+                // Display the file contents by using a foreach loop.
+                WelcomeText.Text = String.Join("\n", lines);
+            }
         }
     }
 }
