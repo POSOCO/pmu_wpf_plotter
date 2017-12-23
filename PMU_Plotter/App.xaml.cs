c9f3eac4766526ca56fff4d1b3d3ee22f0dc2443
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-12-23 14:49:27 +0530

    first commit

---
+++ PMU_Plotter/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PMU_Plotter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // https://stackoverflow.com/questions/26845815/how-to-pass-file-as-parameter-to-your-program-wpf-c-sharp
+            this.Properties["ArbitraryArgName"] = null;
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                var fileName = args[1];
+                if (File.Exists(fileName))
+                {
+                    var extension = Path.GetExtension(fileName);
+                    if (extension == ".sudhir")
+                    {
+                        this.Properties["ArbitraryArgName"] = fileName;
+                    }
+                }
+            }
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ PMU_Plotter/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PMU_Plotter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            String str = (String)((App)Application.Current).Properties["ArbitraryArgName"];
+            if(str != null)
+            {
+                WelcomeText.Text = str;
+            }            
+        }
+
+        public void setWelcomeText(string str)
+        {
+            WelcomeText.Text = str;
+        }
+    }
+}
